af91e5acfbf4e2f8b76acbae85498e64864ea86c
Menelik04 <menelikgete1@gmail.com>
2025-03-30 12:26:00 -0700

    fix: update deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   webpack: (config) => {
     config.externals = [...(config.externals || []), { canvas: 'canvas' }];
     return config;
   },
+  images: {
+    domains: ['github.com', 'raw.githubusercontent.com'],
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
